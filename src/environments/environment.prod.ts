4482046d56904b9904a6f4962520c0412cade6a0
Sravan <sravanreddy.jukanti@gmail.com>
2023-03-04 14:10:30 +0530

    changes

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,8 +1,8 @@
 export const environment = {
   production: true,
   APP_NAME: 'WF',
-  APP_URL: "http://3.111.32.70:3000",
-  SERVER_URL: "http://3.111.32.70:3000/",
-  API_URL: "http://3.111.32.70:3000/api",
+  APP_URL: "http://3.8.166.69:3000",
+  SERVER_URL: "http://3.8.166.69:3000/",
+  API_URL: "http://3.8.166.69:3000/api",
   ENCRPTION_KEY: "19a07b650f031edcd09fb5a1ca6Iw$rvn",
 };
